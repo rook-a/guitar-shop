dbc75bc5d902b9de21525ba38a21b1f9e0e20cd6
rook-a <zhernovkov_konst@mail.ru>
2022-04-29 14:48:41 +0300

    refactor: обновит ссылку на главную в header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,13 +5,13 @@ function Header(): JSX.Element {
   return (
     <header className="header" id="header">
       <div className="container header__wrapper">
-        <Link className="header__logo logo" to={AppRoute.Catalog}>
+        <Link className="header__logo logo" to={AppRoute.Root}>
           <img className="logo__img" width="70" height="70" src="../../img/svg/logo.svg" alt="Логотип" />
         </Link>
         <nav className="main-nav">
           <ul className="main-nav__list">
             <li>
-              <Link className="link main-nav__link link--current" to={AppRoute.Catalog}>
+              <Link className="link main-nav__link link--current" to={AppRoute.Root}>
                 Каталог
               </Link>
             </li>
